14fab3cf7cde48513da5a010211e570f6b5634d9
tks10 <tktktks10@gmail.com>
2019-01-13 12:59:13 +0900

    feat: add QRListFragment

---
+++ app/src/main/java/com/qrist/quicker/qrlist/QRListFragment.kt
@@ -0,0 +1,22 @@
+package com.qrist.quicker.qrlist
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.qrist.quicker.R
+
+
+class QRListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_qrlist, container, false)
+
+        return view
+    }
+}
