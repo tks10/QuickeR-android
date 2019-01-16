dd5cb3cd9e4c76be9c154a9acba9ce3288db716e
tks10 <tktktks10@gmail.com>
2019-01-16 15:40:22 +0900

    update: capture QR code

--- app/src/main/java/com/qrist/quicker/qrlist/QRContainerFragment.kt
+++ app/src/main/java/com/qrist/quicker/qrlist/QRContainerFragment.kt
@@ -1,5 +1,7 @@
 package com.qrist.quicker.qrlist
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
@@ -7,11 +9,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import com.google.zxing.integration.android.IntentIntegrator
 import com.qrist.quicker.R
 import kotlinx.android.synthetic.main.fragment_qrcontainer.view.*
+import com.journeyapps.barcodescanner.CaptureActivity
 
 
 class QRContainerFragment : Fragment() {
+    val RESULT_PICK_QRCODE: Int = 1001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +33,12 @@ class QRContainerFragment : Fragment() {
         view.tool_bar.setOnMenuItemClickListener { item ->
             when (item.itemId) {
                 R.id.menu_capture -> {
+                    val intentIntegrator = IntentIntegrator.forSupportFragment(this).apply {
+                        setPrompt("Scan a QR code")
+                        captureActivity = CaptureActivityPortrait::class.java
+                    }
+                    intentIntegrator.initiateScan()
+
                     Log.d("Menu", "Capture was tapped.")
                     true
                 }
@@ -49,4 +60,17 @@ class QRContainerFragment : Fragment() {
 
         return view
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                RESULT_PICK_QRCODE -> {
+                    val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, resultData)
+                    Log.d("QR result", result.contents)
+                }
+            }
+        }
+    }
 }
+
+class CaptureActivityPortrait : CaptureActivity()
