4cd1ab9eadab989c95479d41afdea97bbe625498
Lucciola <hidenari.t1997nov@gmail.com>
2019-01-16 22:41:19 +0900

    add: test datas

--- app/src/main/java/com/qrist/quicker/MainActivity.kt
+++ app/src/main/java/com/qrist/quicker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.qrist.quicker
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -10,14 +11,48 @@ import android.os.Bundle
 import android.os.Environment
 import android.support.v4.app.ActivityCompat
 import android.widget.Toast
+import com.qrist.quicker.data.QRCodeLocalDataSource
+import com.qrist.quicker.data.QRCodeRepository
 import com.qrist.quicker.extentions.checkPermission
 import com.qrist.quicker.extentions.makeAppDirectory
+import com.qrist.quicker.models.QRCode
+import com.qrist.quicker.models.Service
 import com.qrist.quicker.utils.ImageUtil
 import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     private val directory = File(Environment.getExternalStorageDirectory().absolutePath + "/DCIM/QuickeR/")
+    private val testCode = listOf(
+        QRCode.User(
+            "1",
+            directory.absolutePath + "/qr_code.png",
+            Service.UserService(
+                "1",
+                "user",
+                directory.absolutePath + "/qr_code.png"
+            )
+        ),
+        QRCode.User(
+            "2",
+            directory.absolutePath + "/qr_code.png",
+            Service.UserService(
+                "2",
+                "user2",
+                directory.absolutePath + "/qr_code.png"
+            )
+        ),
+        QRCode.User(
+            "3",
+            directory.absolutePath + "/qr_code.png",
+            Service.UserService(
+                "3",
+                "user3",
+                directory.absolutePath + "/qr_code.png"
+            )
+        )
+    )
+    private val repository: QRCodeRepository by lazy { QRCodeRepository(QRCodeLocalDataSource.getInstance(getSharedPreferences("DataSave", Context.MODE_PRIVATE))) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +72,7 @@ class MainActivity : AppCompatActivity() {
         val bitmap: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.qr_code)
         makeAppDirectory(directory)
         ImageUtil.saveImage(bitmap, directory.absolutePath + "/qr_code.png")
+        repository.saveQRCodes(testCode)
     }
 
 
