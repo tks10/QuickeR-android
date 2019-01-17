d50b33fea55400d693b66aa4656c0563d53680b6
Lucciola <hidenari.t1997nov@gmail.com>
2019-01-17 23:58:06 +0900

    fix: qr view

--- app/src/main/java/com/qrist/quicker/qrlist/QRViewFragmentPagerAdapter.kt
+++ app/src/main/java/com/qrist/quicker/qrlist/QRViewFragmentPagerAdapter.kt
@@ -3,22 +3,27 @@ package com.qrist.quicker.qrlist
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import com.qrist.quicker.models.QRCode
 
 
 class QRViewFragmentPagerAdapter(
-    private val tabTitles: List<String>,
+    private val qrCodes: List<QRCode>,
     fm: FragmentManager
 ) : FragmentPagerAdapter(fm) {
 
     override fun getPageTitle(position: Int): CharSequence? {
-        return tabTitles[position]
+        return when(qrCodes[position]) {
+            is QRCode.Default ->(qrCodes[position] as QRCode.Default).service?.name
+            is QRCode.User -> (qrCodes[position] as QRCode.User).service.name
+            is QRCode.Error -> (qrCodes[position] as QRCode.Error).message
+        }
     }
 
     override fun getItem(position: Int): Fragment? {
-        return QRViewFragment.newInstance(position = position)
+        return QRViewFragment.newInstance(qrCodeId = qrCodes[position].id)
     }
 
     override fun getCount(): Int {
-        return tabTitles.size
+        return qrCodes.size
     }
 }
--- app/src/main/java/com/qrist/quicker/qrlist/QRViewViewModel.kt
+++ app/src/main/java/com/qrist/quicker/qrlist/QRViewViewModel.kt
@@ -1,17 +1,47 @@
 package com.qrist.quicker.qrlist
 
+import android.annotation.SuppressLint
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
-import android.graphics.drawable.Drawable
+import android.content.Context
 import android.util.Log
+import com.qrist.quicker.R
+import com.qrist.quicker.data.QRCodeRepository
+import com.qrist.quicker.models.QRCode
+import com.qrist.quicker.models.Service
+import com.qrist.quicker.utils.convertUrlFromDrawableResId
 
-class QRViewViewModel: ViewModel() {
-    private val imageLiveData: MutableLiveData<Drawable> = MutableLiveData()
-    private val nameLiveData: MutableLiveData<String> = MutableLiveData()
+@SuppressLint("StaticFieldLeak")
+class QRViewViewModel(
+    private val context: Context,
+    private val repository: QRCodeRepository
+): ViewModel() {
+    private val qrCodeImageLiveData: MutableLiveData<String> = MutableLiveData()
+    private val iconImageLiveData: MutableLiveData<String> = MutableLiveData()
 
-    val image: LiveData<Drawable>
-        get() = imageLiveData
-    val name: LiveData<String>
-        get() = nameLiveData
+    val qrCodeImage: LiveData<String>
+        get() = qrCodeImageLiveData
+    val iconImage: LiveData<String>
+        get() = iconImageLiveData
+
+    fun getImageUrl(codeId: String) {
+        val qrCode = repository.getQRCode(codeId)
+        Log.d("imagepath", convertUrlFromDrawableResId(context, R.drawable.qr_code))
+        qrCodeImageLiveData.value = qrCode.qrCodeUrl
+        iconImageLiveData.value = when (qrCode) {
+            is QRCode.User -> qrCode.service.iconUrl
+            is QRCode.Default -> {
+                val service = qrCode.service
+                when (service) {
+                    is Service.TwitterService -> convertUrlFromDrawableResId(context, R.drawable.qr_code)
+                    is Service.FacebookService -> convertUrlFromDrawableResId(context, R.drawable.qr_code)
+                    is Service.LineService -> convertUrlFromDrawableResId(context, R.drawable.qr_code)
+                    // if null TODO: remove ability of UserService from QRCode.Default#service
+                    else -> convertUrlFromDrawableResId(context, R.drawable.qr_code)
+                }
+            }
+            is QRCode.Error -> convertUrlFromDrawableResId(context, R.drawable.qr_code)
+        }
+    }
 }
