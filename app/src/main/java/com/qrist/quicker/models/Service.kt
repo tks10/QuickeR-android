215d080de77bce478d68e24d4c6368aede72f235
Lucciola <hidenari.t1997nov@gmail.com>
2019-01-17 11:59:25 +0900

    delete: iconurl on service

--- app/src/main/java/com/qrist/quicker/models/Service.kt
+++ app/src/main/java/com/qrist/quicker/models/Service.kt
@@ -2,31 +2,31 @@ package com.qrist.quicker.models
 
 import com.squareup.moshi.JsonClass
 
+// if the service is Default one, icon is gonna be included in this app.
 sealed class Service(
     open val id: String,
-    open val name: String,
-    open val iconUrl: String
+    open val name: String
 ) {
 
     @JsonClass(generateAdapter = true)
     data class TwitterService(
         override val id: String
-    ) : Service(id = id, name = "Twitter", iconUrl = "twitter_icon")
+    ) : Service(id = id, name = "Twitter")
 
 
     @JsonClass(generateAdapter = true)
     data class FacebookService(
         override val id: String
-    ) : Service(id = id, name = "Facebook", iconUrl = "facebook_icon")
+    ) : Service(id = id, name = "Facebook")
 
     data class LineService(
         override val id: String
-    ) : Service(id = id, name = "LINE", iconUrl = "line_icon")
+    ) : Service(id = id, name = "LINE")
 
     @JsonClass(generateAdapter = true)
     data class UserService(
         override val id: String,
         override val name: String,
-        override val iconUrl: String
-    ) : Service(id = id, name = name, iconUrl = iconUrl)
+        val iconUrl: String
+    ) : Service(id = id, name = name)
 }
