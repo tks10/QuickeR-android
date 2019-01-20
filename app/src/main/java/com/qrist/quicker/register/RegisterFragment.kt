27f3c0c914214fa2db33955529d6fa907dd4861c
tks10 <tktktks10@gmail.com>
2019-01-20 21:39:40 +0900

    update: register layout

--- app/src/main/java/com/qrist/quicker/register/RegisterFragment.kt
+++ app/src/main/java/com/qrist/quicker/register/RegisterFragment.kt
@@ -19,6 +19,8 @@ class RegisterFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_register, container, false)
 
+
+
         return view
     }
 }
