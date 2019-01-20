1c2cd1b8e86b45fa99e898712949d9bf6ae80203
tks10 <tktktks10@gmail.com>
2019-01-20 18:15:22 +0900

    feat: recycler view binding

---
+++ app/src/main/java/com/qrist/quicker/models/ServiceListViewer.kt
@@ -0,0 +1,7 @@
+package com.qrist.quicker.models
+
+data class ServiceListViewer(
+    val serviceName: String,
+    val serviceIconUrl: String,
+    val isRegistered: Boolean
+)
---
+++ app/src/main/java/com/qrist/quicker/servicelist/ServiceListAdapter.kt
@@ -0,0 +1,41 @@
+package com.qrist.quicker.servicelist
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.qrist.quicker.databinding.ServicelistItemBinding
+import com.qrist.quicker.models.ServiceListViewer
+
+class ServiceListAdapter(val context: Context, private val serviceListViewers: List<ServiceListViewer>)
+    : RecyclerView.Adapter<ServiceListAdapter.ViewHolder>() {
+    private var listener: View.OnClickListener? = null
+
+    class ViewHolder(val binding: ServicelistItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ServicelistItemBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    fun setOnItemClickListener(listener: View.OnClickListener) {
+        this.listener = listener
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // Binding
+        holder.binding.serviceListViewer = serviceListViewers[position]
+
+        // Set Click Listener
+        holder.binding.root.id = holder.adapterPosition
+        holder.binding.root.setOnClickListener {
+            listener?.onClick(it)
+        }
+    }
+
+    override fun getItemCount() = serviceListViewers.size
+}
