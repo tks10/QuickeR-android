8a408181d37e82247f1f019727d37bcb675a5b9a
tks10 <tktktks10@gmail.com>
2019-01-28 12:56:25 +0900

    feat: take margin around QR Code

--- app/src/main/java/com/qrist/quicker/extentions/BitmapExtensions.kt
+++ app/src/main/java/com/qrist/quicker/extentions/BitmapExtensions.kt
@@ -3,13 +3,19 @@ package com.qrist.quicker.extentions
 import android.graphics.Bitmap
 import android.graphics.Matrix
 import android.graphics.Rect
+import kotlin.math.max
+import kotlin.math.min
 
-fun Bitmap.trim(start: Int, top: Int, end: Int, bottom: Int): Bitmap {
+fun Bitmap.trim(start: Int, top: Int, end: Int, bottom: Int, margin: Int = 10): Bitmap {
     val width = end - start
     val height = bottom - top
-    return Bitmap.createBitmap(this, start, top, width, height, Matrix(), true)
+    val s = max(0, start - margin)
+    val t = max(0, top - margin)
+    val w = min(this.width - start - 1, width + margin * 2)
+    val h = min(this.height - top - 1, height + margin * 2)
+    return Bitmap.createBitmap(this, s, t, w, h, Matrix(), true)
 }
 
-fun Bitmap.trim(rect: Rect): Bitmap {
-    return this.trim(rect.left, rect.top, rect.right, rect.bottom)
+fun Bitmap.trim(rect: Rect, margin: Int = 10): Bitmap {
+    return this.trim(rect.left, rect.top, rect.right, rect.bottom, margin)
 }
