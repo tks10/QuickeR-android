2b454e5f6c91428df38df3c885c3e81c9528afbb
Lucciola <hidenari.t1997nov@gmail.com>
2019-02-06 02:07:22 +0900

    rewrite: viewpager

--- app/src/main/java/com/qrist/quicker/qrlist/QRViewFragmentPagerAdapter.kt
+++ app/src/main/java/com/qrist/quicker/qrlist/QRViewFragmentPagerAdapter.kt
@@ -1,24 +1,84 @@
 package com.qrist.quicker.qrlist
 
+import android.annotation.SuppressLint
 import android.os.Parcelable
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
+import android.support.v4.view.PagerAdapter
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
 import com.qrist.quicker.models.QRCode
-import com.qrist.quicker.qrlist.widgets.QRViewFragmentStatePagerAdapter
 
 class QRViewFragmentPagerAdapter(
     private var qrCodes: List<QRCode>,
     private val fm: FragmentManager
-) : QRViewFragmentStatePagerAdapter(fm) {
+) : PagerAdapter() {
 
-    fun getCenterPosition(position: Int): Int = qrCodes.size * NUMBER_OF_LOOPS / 2 + position
+    private var currentTransaction: FragmentTransaction? = null
+    private var currentFragment: Fragment? = null
 
-    private fun getValueAt(position: Int): QRCode? =
-        when (qrCodes.size) {
-            0 -> null
-            else -> qrCodes[position % qrCodes.size]
+    // begin lifecycle
+
+    override fun startUpdate(container: ViewGroup) {
+        if (container.id == -1) {
+            throw IllegalStateException("ViewPager with adapter $this requires a view id")
         }
+        Log.d("fragment", "start update!")
+    }
+
+    @SuppressLint("CommitTransaction")
+    override fun instantiateItem(container: ViewGroup, position: Int): Any =
+        fm.findFragmentByTag(getItemId(position)).let { fragment ->
+            currentTransaction ?: fm.beginTransaction().also {
+                currentTransaction = it
+            }
+            when (fragment) {
+                null -> {
+                    val returnVal = getItem(position)!!
+                    currentTransaction!!.add(container.id, returnVal, getItemId(position))
+                    returnVal
+                }
+                else -> {
+                    currentTransaction!!.attach(fragment)
+                    fragment
+                }
+            }.apply {
+                if (fragment != currentFragment) {
+                    fragment?.setMenuVisibility(false)
+                    fragment?.userVisibleHint = false
+                }
+                Log.d("fragment init item", fragment.toString())
+            }
+        }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        Log.d("delete fragment", `object`.toString())
+    }
+
+    override fun setPrimaryItem(container: ViewGroup, position: Int, `object`: Any) {
+        val fragment = `object` as Fragment
+        if (fragment != currentFragment) {
+            fragment.setMenuVisibility(true)
+            fragment.userVisibleHint = true
+            this.currentFragment = fragment
+            Log.d("fragment primary item", fragment.toString())
+        }
+    }
+
+    override fun finishUpdate(container: ViewGroup) {
+        currentTransaction?.commitNowAllowingStateLoss().also {
+            currentTransaction = null
+        }
+        Log.d("fragment pager adapter", "update finished!")
+    }
+
+    // finish lifecycle
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return (`object` as Fragment).view == view
+    }
 
     override fun getCount(): Int =
         (qrCodes.size * NUMBER_OF_LOOPS).let {
@@ -28,23 +88,38 @@ class QRViewFragmentPagerAdapter(
             }
         }
 
-    override fun getItem(position: Int): Fragment? =
+    @SuppressLint("CommitTransaction")
+    fun detachItems() {
+        qrCodes.forEach { code ->
+            fm.findFragmentByTag(code.id)?.also {fragment ->
+                currentTransaction ?: fm.beginTransaction().also { transaction ->
+                    currentTransaction = transaction
+                }.detach(fragment)
+            }
+        }
+    }
+
+    fun getCenterPosition(position: Int): Int = qrCodes.size * NUMBER_OF_LOOPS / 2 + position
+
+    private fun getValueAt(position: Int): QRCode? =
+        when (qrCodes.size) {
+            0 -> null
+            else -> qrCodes[position % qrCodes.size]
+        }
+
+    private fun getItem(position: Int): Fragment? =
         getValueAt(position)?.let {
             QRViewFragment.newInstance(qrCodeId = it.id)
         }
 
-    override fun getItemId(position: Int): String? =
-        getValueAt(position)?.let {
-            it.id
-        }
+    private fun getItemId(position: Int): String? =
+        getValueAt(position)?.id
 
-    fun detachItems() {
-        qrCodes.forEach {
-            deleteItems(it.id)
-        }
-    }
 
     override fun saveState(): Parcelable? = null
+
+    override fun restoreState(state: Parcelable?, loader: ClassLoader?) {
+    }
 
     companion object {
         private const val NUMBER_OF_LOOPS = 10000
