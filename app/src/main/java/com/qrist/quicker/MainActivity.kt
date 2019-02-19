db2946b93574f8fa59e44b7ba18e4bd240e9b4bf
Lucciola <hidenari.t1997nov@gmail.com>
2019-02-19 23:29:37 +0900

    fix: MainActivity to use normal activity

--- app/src/main/java/com/qrist/quicker/MainActivity.kt
+++ app/src/main/java/com/qrist/quicker/MainActivity.kt
@@ -21,37 +21,5 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(navController.graph)
         findViewById<Toolbar>(R.id.tool_bar)
             .setupWithNavController(navController, appBarConfiguration)
-
-        handleAndConsumeIntentOnSend()
-    }
-
-    override fun onNewIntent(intent: Intent?) {
-        super.onNewIntent(intent)
-        Log.d("Intent", "onNewIntent")
-        handleAndConsumeIntentOnSend(receivedIntent = intent)
-    }
-
-    private fun handleAndConsumeIntentOnSend(receivedIntent: Intent? = null) {
-        // Get extras if intent is given by the argument, otherwise get from activity.intent
-        val intent = receivedIntent ?: this.intent
-        Log.d("Intent", receivedIntent.toString() + this.intent.toString())
-        val extras = intent.extras
-
-        // Check whether or not intent is from sharing.
-        extras?.get(INTENT_BUNDLE_KEY)?.let {
-            Log.d("Intent", "Intent from other service:, value: $it")
-
-            // Clear Intent
-            this@MainActivity.intent = null
-
-            val action =
-                QRContainerFragmentDirections.actionQrContainerToServiceaddlist(it.toString())
-            Navigation.findNavController(this, R.id.nav_host_fragment).popBackStack(R.id.qrContainerFragment, false)
-            Navigation.findNavController(this, R.id.nav_host_fragment).navigate(action)
-        }
-    }
-
-    companion object {
-        const val INTENT_BUNDLE_KEY = "android.intent.extra.STREAM"
     }
 }
