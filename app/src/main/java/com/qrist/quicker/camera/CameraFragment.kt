eed6488d3528793ecf8545eebcf3a1ea76a8caa5
Lucciola <hidenari.t1997nov@gmail.com>
2019-03-03 12:42:13 +0900

    add: permission verification process in camerafragment

--- app/src/main/java/com/qrist/quicker/camera/CameraFragment.kt
+++ app/src/main/java/com/qrist/quicker/camera/CameraFragment.kt
@@ -1,11 +1,17 @@
 package com.qrist.quicker.camera
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.view.View
+import androidx.navigation.Navigation
+
 import com.qrist.quicker.R
+import com.qrist.quicker.extentions.checkCameraPermission
+import com.qrist.quicker.extentions.requestPermission
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_camera.*
 
@@ -23,11 +29,31 @@ class CameraFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-        cameraPreview.startCameraPreview()
+        if (!checkCameraPermission()) {
+            requestPermission(Manifest.permission.CAMERA, REQUEST_PERMISSION_ON_CAMERA)
+        } else {
+            cameraPreview.startCameraPreview()
+        }
     }
 
     override fun onPause() {
         super.onPause()
-        cameraPreview.stopCameraPreview()
+        if (checkCameraPermission()) {
+            cameraPreview.stopCameraPreview()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+            Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
+                .popBackStack(R.id.qrContainerFragment, false)
+        } else {
+            cameraPreview.startCameraPreview()
+        }
+    }
+
+    companion object {
+        private const val REQUEST_PERMISSION_ON_CAMERA = 1
     }
 }
