5cb4d9e13840fefb7e792ec1436d7626fe45f8b6
tks10 <tktktks10@gmail.com>
2019-03-30 22:55:56 +0900

    fix: use content resolver

--- app/src/main/java/com/qrist/quicker/utils/ImageUtil.kt
+++ app/src/main/java/com/qrist/quicker/utils/ImageUtil.kt
@@ -76,11 +76,13 @@ fun getDrawableFromUri(uri: Uri): Drawable {
 
 fun getBitmapFromUri(context: Context, uri: Uri): Bitmap {
     Log.d("ImageUtil", uri.toString())
-    return if (uri.toString().matches("^content://[^/]*[Ff]ile[Pp]rovider[^/]*/.*".toRegex())) {
-        val fileDescriptor = FileProvider().openFile(uri, "r")?.fileDescriptor
-        BitmapFactory.decodeFileDescriptor(fileDescriptor).copy(Bitmap.Config.ARGB_8888, true)
-    } else {
-        val inputStream: InputStream? = context.contentResolver.openInputStream(uri)
-        BitmapFactory.decodeStream(BufferedInputStream(inputStream)).copy(Bitmap.Config.ARGB_8888, true)
-    }
+//    return if (uri.toString().matches("^content://[^/]*[Ff]ile[Pp]rovider[^/]*/.*".toRegex()) && true) {
+//        val fileDescriptor = context.contentResolver.openFileDescriptor(uri, "r")?.fileDescriptor
+//        BitmapFactory.decodeFileDescriptor(fileDescriptor).copy(Bitmap.Config.ARGB_8888, true)
+//    } else {
+//        val inputStream: InputStream? = context.contentResolver.openInputStream(uri)
+//        BitmapFactory.decodeStream(BufferedInputStream(inputStream)).copy(Bitmap.Config.ARGB_8888, true)
+//    }
+    val fileDescriptor = context.contentResolver.openFileDescriptor(uri, "r")?.fileDescriptor
+    return BitmapFactory.decodeFileDescriptor(fileDescriptor).copy(Bitmap.Config.ARGB_8888, true)
 }
