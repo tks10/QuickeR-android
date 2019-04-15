069c29111bf7cebf14d11ddfb084303c0d76316e
clockvoid <hidenari.t1997nov@gmail.com>
2019-04-16 00:13:12 +0900

    add: test for QRViewViewModel

--- app/src/test/java/com/qrist/quicker/qrlist/QRViewViewModelTest.kt
+++ app/src/test/java/com/qrist/quicker/qrlist/QRViewViewModelTest.kt
@@ -1,5 +1,35 @@
 package com.qrist.quicker.qrlist
 
-import org.junit.jupiter.api.Assertions.*
+import android.app.Application
+import com.qrist.quicker.InstantExecutorExtension
+import com.qrist.quicker.data.QRCodeRepository
+import com.qrist.quicker.models.QRCode
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
-internal class QRViewViewModelTest
+@ExtendWith(InstantExecutorExtension::class)
+internal class QRViewViewModelTest {
+
+    @Test
+    fun basicTest() {
+        // Arrange
+        val mockedRepo = mockk<QRCodeRepository>(relaxed = true)
+        val mockedApp = mockk<Application>(relaxed = true)
+        val viewModel = QRViewViewModel(mockedApp, mockedRepo)
+        every { mockedRepo.getQRCode("test") } returns
+                QRCode.User(
+                    "1",
+                    "https://hoge.com",
+                    "test",
+                    "test")
+
+        // Act
+        viewModel.fetchImageUrl("test")
+
+        // Assert
+        verify { mockedRepo.getQRCode("test") }
+    }
+}
